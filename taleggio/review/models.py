618e1d097615ca9a06b0e0ac73c078037cde9cc6
Chris Bednar <bednar.christopher@gmail.com>
2015-04-12 07:23:02 -0400

    metamind

--- taleggio/review/models.py
+++ taleggio/review/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import User
+import random
 
 from dwelling.models import Dwelling
 
@@ -51,7 +52,24 @@ class Review(models.Model):
       return self.userCreated.__str__() + " - " + self.content[:10]
 
    def save(self, *args, **kwargs):
-        super(Review, self).save(*args, **kwargs) # Call the "real" save() method.
+      from metamind.api import ClassificationModel, set_api_key
+      set_api_key("JcsbHMxoLMdw70XN9XxSWH03H69yYKA8Ruad6B28lA1kgY0s4t")
+      prediction = ClassificationModel(id=88).predict(self.content, input_type="text")
+
+      rint = random.randint(0,1)
+
+      if (prediction[0].get('label') == 'negative'):
+            self.rating = .5 + rint
+      else:
+         if (prediction[0].get('probability') < .4):
+            if (rint == 0):
+               self.rating = 2.5
+            else:
+               self.rating = 3.5
+         else:
+            self.rating = 5 - rint
+
+      super(Review, self).save(*args, **kwargs) # Call the "real" save() method.
 
 
 class DwellingReview(Review):
